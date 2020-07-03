4c2bb4e65e93184e018cc367448f2a164afc7b71
Shashank Gaur <gaurshashank014@gmail.com>
2020-07-03 21:24:22 +0530

    save

--- controllers/auth.js
+++ controllers/auth.js
@@ -78,12 +78,6 @@ exports.signout = (req, res) => {
   });
 };
 
-//protected routes
-// exports.isSignedIn = expressJwt({
-//   secret: process.env.SECRET,
-//   userProperty: "auth"
-// });
-
 //custom middlewares
 exports.isAuthenticated = (req, res, next) => {
   let checker = req.profile && req.auth && req.profile._id == req.auth._id;
