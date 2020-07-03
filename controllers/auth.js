aee6289442ef0e9a34a20fb99380d99159e0cfb5
Shashank Gaur <gaurshashank014@gmail.com>
2020-07-03 21:10:57 +0530

    save

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const User = require("../models/user");
 const { check, validationResult } = require("express-validator");
 var jwt = require("jsonwebtoken");
-var expressJwt = require("express-jwt");
 
 const formidable = require("formidable");
 const _ = require("lodash");
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const { check, validationResult } = require("express-validator");
-const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
+const { signout, signup, signin } = require("../controllers/auth");
 
 router.post(
   "/signup",
